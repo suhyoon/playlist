3255da0df6366287493a02cf7c903dc82c900347
suhyoon <suh@scripted.org>
2020-02-27 19:46:25 +0000

    let

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /* global $ */
 
 
-var songs = [
+let songs = [
     "I Should Live in Salt", 
     "True Love Waits",
     "Shallow",
@@ -10,32 +10,33 @@ var songs = [
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
 // Make sure they match the same order as your array above
 
-var images_links=[
+let images_links=[
     "https://images.genius.com/b904ca605fa6dd5d417cb1d569d4cbbd.1000x1000x1.jpg", 
     "https://img.discogs.com/1Fz2fj-D-QMQD39IQP727i2mbNI=/discogs-uploads/u4303-radiohead-moonshaped.jpg.jpg", 
     "https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/A_Star_Is_Born_%282018_soundtrack%29.png/220px-A_Star_Is_Born_%282018_soundtrack%29.png", 
     "http://cdn.shopify.com/s/files/1/0812/1837/products/Tangled_a8266244-389d-42cb-8800-c8e8477bcf5c_grande.jpg?v=1449610456"
     ];
 
-var artists=[
+let artists=[
     "The National", 
     "Radiohead", 
     "Lady Gaga, Bradley Cooper",
     "Thomas Rhett"
     ];
 
-var songlengths=[
+let songlengths=[
     "4:08",
     "3:35",
     "4:43",
     "3:47"
     ];
-var links=[
+let links=[
     "https://open.spotify.com/search/results/i%20should%20live%20in%20salt",
     "https://open.spotify.com/search/results/true%20love%20waits",
     "https://open.spotify.com/search/results/shallow",
     "https://open.spotify.com/album/4ykbJPI8MPscFzROE8dqLy"
     ];
+
 
 
 function displaySongInfo(){
@@ -74,19 +75,19 @@ function emptySongInfo(){
 }
 
 function addSongInfo(){
-    var songInput = $("#song").val();
+    let songInput = $("#song").val();
      songs.push(songInput);
      
-    var imageInput = $("#image").val();
+    let imageInput = $("#image").val();
      images_links.push(imageInput);
      
-    var artistInput = $("#artist").val();
+    let artistInput = $("#artist").val();
      artists.push(artistInput);
     
-    var lengthInput = $("#length").val();
+    let lengthInput = $("#length").val();
      songlengths.push(lengthInput);
      
-    var linkInput = $("#link").val();
+    let linkInput = $("#link").val();
      links.push(linkInput)
 
 }
